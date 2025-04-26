7e51d5e05ebb6ef6bb2ba5cb3bfc7f9193983f8d
yokoyama_tetsu <t.y.develop.1021@gmail.com>
2025-04-26 20:08:19 +0900

    元に戻す

--- cmd/emotion_analysis/main.go
+++ cmd/emotion_analysis/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"log"
-
 	"github.com/TetsuYokoyamaDevelop/emotion_analysis.git/config"
-	"github.com/TetsuYokoyamaDevelop/emotion_analysis.git/internal/model"
 	"github.com/TetsuYokoyamaDevelop/emotion_analysis.git/routes"
-	"gorm.io/gorm"
 )
 
 func main() {
 	config.LoadEnv()
 	db := config.InitDB()
-	if err := db.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&model.Message{}).Error; err != nil {
-		log.Fatalf("Messageテーブルの削除に失敗しました: %v", err)
-	}
 	engine := routes.SetupRouter(db)
 	engine.Run("0.0.0.0:3000")
 }
